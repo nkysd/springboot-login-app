7abc7bab34d19fba373b9b120b31e306df9f6178
Mio <m.mizutaniii@gmail.com>
2026-01-21 21:15:20 +0900

    chore: initialize Spring Boot project

---
+++ src/main/java/com/example/springbootlogin/app/SpringbootLoginAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootlogin.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLoginAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootLoginAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootlogin/app/SpringbootLoginAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootlogin.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootLoginAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
